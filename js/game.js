f91d2ce0c94184b45942e169e7174df3f8c3771a
szymongembalczyk <szymon.gembalczyk@ekookna.pl>
2021-10-05 10:54:03 +0200

    ship movement added

---
+++ js/game.js
@@ -0,0 +1,19 @@
+import {Spaceship} from './spaceship.js'
+
+class Game{
+  #htmlElements = {
+    spaceship: document.querySelector('[data-spaceship]'),
+  };
+
+  #ship = new Spaceship(this.#htmlElements.spaceship);
+
+
+  init(){
+    this.#ship.init();
+  }
+}
+
+window.onload = function(){
+  const game = new Game();
+  game.init();
+}
