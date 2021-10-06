7a0aace28b54b370f8dc9de1ebb58ab914d89f7c
szymongembalczyk <szymon.gembalczyk@ekookna.pl>
2021-10-06 11:44:58 +0200

    Enemies rendering functionality

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
-import { Spaceship } from './spaceship.js'
-
+import { Spaceship } from './spaceship.js';
+import { Enemy } from './Enemy.js';
 class Game {
   #htmlElements = {
     spaceship: document.querySelector('[data-spaceship]'),
@@ -11,16 +11,54 @@ class Game {
     this.#htmlElements.container
     );
 
+
+  #enemies = [];
+  #enemiesInterval = null;
+  #checkPositionInterval = null;
+  #createEnemyInterval = null;
+
   init() {
     this.#ship.init();
     this.#newGame();
   }
 
   #newGame(){
-    setInterval(() => this.#checkPosition(), 1);
+    this.#enemiesInterval = 30;
+    this.#createEnemyInterval = setInterval(() => this.#randomNewEnemy(), 1000);
+    this.#checkPositionInterval = setInterval(() => this.#checkPosition(), 1);
+  }
+
+  #randomNewEnemy(){
+    const randomNumber = Math.floor(Math.random() * 5) + 1;
+    randomNumber % 5 
+    ? this.#createNewEnemy(this.#htmlElements.container, this.#enemiesInterval, 'enemy') 
+    : this.#createNewEnemy(this.#htmlElements.container, this.#enemiesInterval * 2, 'enemy--big')
+  }
+
+  #createNewEnemy(...params){
+    const enemy = new Enemy(...params);
+
+    enemy.init();
+    this.#enemies.push(enemy);
   }
 
   #checkPosition() {
+    this.#enemies.forEach((enemy, enemyIndex, enemiesArray) => {
+     
+      const enemyPosition = {
+        top: enemy.element.offsetTop,
+        right: enemy.element.offsetLeft + enemy.element.offsetWidth,
+        bottom: enemy.element.offsetTop + enemy.element.offsetHeight,
+        left: enemy.element.offsetLeft
+      };
+
+      if (enemyPosition.top > window.innerHeight) {
+        enemy.remove();
+        enemiesArray.splice(enemyIndex,1)
+      }
+
+    })
+
     this.#ship.missiles.forEach((missile, missileIndex, missileArray) => {
       const missilePosition = {
         top: missile.element.offsetTop,
@@ -31,7 +69,9 @@ class Game {
 
       if (missilePosition.bottom < 0) {
         missile.remove();
+        missileArray.splice(missileIndex,1)
       }
+      
     })
   }
 }
